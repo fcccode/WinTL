70304e7105be622fa57567054dc34e4e60d4e4ff
Putta Khunchalee <ultimaweapon@outlook.com>
2015-04-25 14:50:54 +0700

    Reformat include/win32/service.hpp

--- include/win32/service.hpp
+++ include/win32/service.hpp
@@ -318,39 +318,45 @@ static const std::uint8_t service_main_trunk[] = {
 
 class service final {
 public:
-    service(const std::wstring& name,
-            const std::function<void(int, wchar_t **)>& proc) : name_(name),
-            proc_(proc)
+    service(
+            const std::wstring& name,
+            const std::function<void(int, wchar_t **)>& proc) :
+                    name_(name),
+                    proc_(proc)
     {
-        proctrunk_ = reinterpret_cast<std::uint8_t *>(::VirtualAlloc(nullptr,
-                sizeof(service_main_trunk), MEM_COMMIT | MEM_RESERVE,
+        proctrunk_ = reinterpret_cast<std::uint8_t *>(VirtualAlloc(
+                nullptr,
+                sizeof(service_main_trunk),
+                MEM_COMMIT | MEM_RESERVE,
                 PAGE_EXECUTE_READWRITE));
+
         if (!proctrunk_) {
-            auto err = ::GetLastError();
+            auto err = GetLastError();
             throw std::system_error(err, std::system_category());
         }
 
         std::memcpy(proctrunk_, service_main_trunk, sizeof(service_main_trunk));
-
-        reinterpret_cast<std::intptr_t *>(proctrunk_ +
-                SERVICE_MAIN_TRUNK_THIS_OFFSET)[0] =
+        reinterpret_cast<std::intptr_t *>(
+                proctrunk_ + SERVICE_MAIN_TRUNK_THIS_OFFSET)[0] =
                 reinterpret_cast<std::intptr_t>(this);
 
         union {
-            VOID (WINAPI service::*x)(DWORD, LPWSTR *);
+            VOID (WINAPI service::*ptr)(DWORD, LPWSTR *);
             std::intptr_t addr;
         } svcmain = {&service::service_boot};
 
-        reinterpret_cast<std::intptr_t *>(proctrunk_ +
-                SERVICE_MAIN_TRUNK_PROC_OFFSET)[0] = svcmain.addr;
+        reinterpret_cast<std::intptr_t *>(
+                proctrunk_ + SERVICE_MAIN_TRUNK_PROC_OFFSET)[0] = svcmain.addr;
     }
 
-    service(service&& src) : name_(std::move(src.name_)),
-            proc_(std::move(src.proc_)), proctrunk_(src.proctrunk_)
+    service(service&& src) :
+            name_(std::move(src.name_)),
+            proc_(std::move(src.proc_)),
+            proctrunk_(src.proctrunk_)
     {
         src.proctrunk_ = nullptr;
-        reinterpret_cast<std::intptr_t *>(proctrunk_ +
-                SERVICE_MAIN_TRUNK_THIS_OFFSET)[0] =
+        reinterpret_cast<std::intptr_t *>(
+                proctrunk_ + SERVICE_MAIN_TRUNK_THIS_OFFSET)[0] =
                 reinterpret_cast<std::intptr_t>(this);
     }
 
@@ -358,26 +364,30 @@ public:
 
     ~service()
     {
-        if (proctrunk_) ::VirtualFree(proctrunk_, 0, MEM_RELEASE);
+        if (proctrunk_) {
+            VirtualFree(proctrunk_, 0, MEM_RELEASE);
+        }
     }
 
-    service& operator=(service&& src)
+    service& operator = (service&& src)
     {
-        if (proctrunk_) ::VirtualFree(proctrunk_, 0, MEM_RELEASE);
+        if (proctrunk_) {
+            VirtualFree(proctrunk_, 0, MEM_RELEASE);
+        }
 
         name_ = std::move(src.name_);
         proc_ = std::move(src.proc_);
         proctrunk_ = src.proctrunk_;
         src.proctrunk_ = nullptr;
 
-        reinterpret_cast<std::intptr_t *>(proctrunk_ +
-                SERVICE_MAIN_TRUNK_THIS_OFFSET)[0] =
+        reinterpret_cast<std::intptr_t *>(
+                proctrunk_ + SERVICE_MAIN_TRUNK_THIS_OFFSET)[0] =
                 reinterpret_cast<std::intptr_t>(this);
 
         return *this;
     }
 
-    service& operator=(const service&) = delete;
+    service& operator = (const service&) = delete;
 
     LPSERVICE_MAIN_FUNCTIONW bootstrapper() const
     {
@@ -393,9 +403,9 @@ private:
     std::function<void(int, wchar_t **)> proc_;
     std::uint8_t *proctrunk_;
 
-    void service_boot(DWORD dwArgc, LPWSTR *lpszArgv)
+    void service_boot(DWORD argc, LPWSTR *argv)
     {
-        proc_(dwArgc, lpszArgv);
+        proc_(argc, argv);
     }
 };
 
@@ -419,8 +429,8 @@ inline void service_control_dispatcher(InputIt first, InputIt last)
         table.push_back(svc);
     }
 
-    if (!::StartServiceCtrlDispatcherW(table.data())) {
-        auto err = ::GetLastError();
+    if (!StartServiceCtrlDispatcherW(table.data())) {
+        auto err = GetLastError();
         throw std::system_error(err, std::system_category());
     }
 }
